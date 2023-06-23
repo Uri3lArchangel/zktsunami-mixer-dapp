6d903d50695b6cbf078513d228c1b6936b8ac6e9
root <root@Uri3lArchangel>
2023-06-22 21:13:45 -0400

    type error fixed

--- src/BE/functions/Deposit.js
+++ src/BE/functions/Deposit.js
@@ -1,17 +1,17 @@
-import Web3 from "web3"
+import Web3, { ContractMethod } from "web3"
 import mixerJSON from '../build/ZkTsunamiMixer.json'
 import { splitNumber } from "./shift"
-import {proofDeposit} from '../functions/zokratesDeposit'
+import {proofDeposit} from './zokratesDeposit'
 import { ethers } from "ethers"
 
 const addr = "0x2952448d21f2Ee6B0253F75aE78c5231E7B48e47"
 
-export const web3Deposit=async(amount:number,key:string)=>{
+export const web3Deposit=async(amount,key)=>{
 const provider =  new ethers.BrowserProvider(window.ethereum) 
 const web3 = new Web3(window.ethereum)
 const signer = await provider.getSigner()
 const a = signer.address
-const contract = new web3.eth.Contract<any>(mixerJSON.output.abi,addr)
+const contract = new web3.eth.Contract(mixerJSON.output.abi,addr)
 
 
 let p = await (contract.methods.depositBase(Web3.utils.toNumber(String((amount) * parseInt('10000000000000000'))))).send({from:a,value:String((amount) * parseInt('10000000000000000'))})
--- src/BE/functions/Withdrawal.js
+++ src/BE/functions/Withdrawal.js
@@ -4,13 +4,13 @@ import mixerJSON from '../build/ZkTsunamiMixer.json'
 
 const addr = "0x2952448d21f2Ee6B0253F75aE78c5231E7B48e47"
 
-export const WitdrawContract=async(addresses:String[],amounts:String[],xy:String[],proof:any)=>{
+export const WitdrawContract=async(addresses,amounts,xy,proof)=>{
     const provider =  new ethers.BrowserProvider(window.ethereum) 
     const web3 = new Web3(window.ethereum)
     const signer = await provider.getSigner()
     const a = signer.address
     const Proof = JSON.parse(proof.status)
-    const contract = new web3.eth.Contract<any>(mixerJSON.output.abi,addr)
+    const contract = new web3.eth.Contract(mixerJSON.output.abi,addr)
     let p = await contract.methods.withdrawBase(addresses,amounts,xy,Proof).send({from:a})
         
 }

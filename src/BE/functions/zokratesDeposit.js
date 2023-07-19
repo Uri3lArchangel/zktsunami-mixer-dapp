d7331e53a561489e9f71005507aebd209ed0bdc0
root <root@Uri3lArchangel>
2023-07-19 11:21:26 -0400

    test

--- src/BE/functions/zokratesDeposit.js
+++ src/BE/functions/zokratesDeposit.js
@@ -4,13 +4,7 @@ console.log(1)
   const zokratesProvider = await zokrates.initialize();
   console.log(2)
 
-  const source = `import "hashes/sha256/512bitPacked" as shaPacked;
-   def main(private field a,private field b,private field c,private field d) -> field[2]
-  {
-    field[2] dp= shaPacked([a,b,c,d]);return dp;
-  }`
-  
-  ;
+  const source = 'import "hashes/sha256/512bitPacked" as shaPacked;def main(private field a,private field b,private field c,private field d) -> field[2]{field[2] dp= shaPacked([a,b,c,d]);return dp;}'  ;
       console.log(3)
 
   const artifacts = zokratesProvider.compile((source));
